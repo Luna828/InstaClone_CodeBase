059f0739f5de238aeb8edae3126910831dbc0bfc
Luna828 <eskw0701@naver.com>
2023-09-12 10:51:47 +0900

    [FEAT] project 생성

---
+++ InstaClone_CodeBase/InstaClone_CodeBase/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone_CodeBase
+//
+//  Created by t2023-m0050 on 2023/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
