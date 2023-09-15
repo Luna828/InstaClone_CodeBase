54644be8bc11595db22255d903a294f0988a9c33
Luna828 <eskw0701@naver.com>
2023-09-15 22:14:53 +0900

    [FEAT]: Error 해결

--- InstaClone_CodeBase/Todo+CoreDataProperties.swift
+++ InstaClone_CodeBase/Todo+CoreDataProperties.swift
@@ -1,17 +1,28 @@
+//
+//  Todo+CoreDataProperties.swift
+//  InstaClone_CodeBase
+//
+//  Created by t2023-m0050 on 2023/09/15.
+//
+//
+
 import Foundation
 import CoreData
 
-@objc(Todo)
-public class Todo: NSManagedObject {}
 
 extension Todo {
+
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Todo> {
         return NSFetchRequest<Todo>(entityName: "Todo")
     }
 
-    @NSManaged public var id: UUID
     @NSManaged public var content: String?
     @NSManaged public var date: Date?
+    @NSManaged public var id: UUID?
     @NSManaged public var isChecked: Bool
+
+}
+
+extension Todo : Identifiable {
 
 }
